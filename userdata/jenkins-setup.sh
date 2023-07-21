5191c34ffa175b9b1fbd52fc800ed465a1ad9dfd
SAK <120071904+sakit333@users.noreply.github.com>
2023-07-21 14:38:47 +0530

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -7,3 +7,37 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo apt update -y
 sudo apt install openjdk-11* -y 
+
+or 
+
+#!/bin/bash
+sudo apt update
+sudo apt install maven git wget unzip -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install jenkins -y
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+###
+
+if it is not working use this one --> recomended use this one only for project
+> vi jenkins.sh
+
+#!/bin/bash
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt update -y
+sudo apt install openjdk-11-jdk -y
+java --version
+openjdk version "11.0.12" 2021-07-20
+OpenJDK Runtime Environment (build 11.0.12+7-post-Debian-2)
+OpenJDK 64-Bit Server VM (build 11.0.12+7-post-Debian-2, mixed mode, sharing)
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+:wq → sh jenkins.sh → jenkins --version → jenkins will be installed
